63cf1941d3448c2982ca23b65d7b36fb6c4a8566
thiago <160151673+Thiagolyx@users.noreply.github.com>
2024-11-21 11:54:45 -0300

    native_telas

---
+++ src/mocks/textos.js
@@ -0,0 +1,13 @@
+const textos = {
+    topoCesta: "Detalhe da cesta",
+    botaoComprar: "Comprar",
+    tituloItens: "Itens da cesta",
+    mensagemCompra: "Agradecemos pela compra da cesta $NOME, seu pedido chegará em breve!",
+    mensagemCompra: "Agradecemos pela compra da cesta $NOME, seu pedido chegará em breve!",
+    topoCompra: "Pedido feito com sucesso!",
+    tituloCompra: "Parabéns!",
+    botaoHomeCompra: "Voltar a Home",
+    botaoProdutorCompra: "Voltar ao Produtor",
+};
+
+export default textos;
